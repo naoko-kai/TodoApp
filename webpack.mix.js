dde4af478b8fe52817e2b9f9a4aadf1db05d3f19
Naoko Kai <n_kai@goldswan.co.jp>
2021-12-12 23:58:24 +0900

    ReactとTypeScriptのインストール

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,11 +7,13 @@ const mix = require('laravel-mix');
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
  | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
+ | file for the application as well as bundling up all the TS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.ts('resources/ts/index.tsx', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}

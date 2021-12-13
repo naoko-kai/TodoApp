ec18390b1d406972f2134bede108097c6b65f742
Naoko Kai <n_kai@goldswan.co.jp>
2021-12-13 17:52:04 +0900

    React Router でのページ分け

--- resources/ts/index.tsx
+++ resources/ts/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const App = () => {
-  return (
-    <h1>Laravel SPA</h1>
-  )
-}
+import App from './app'
 
 ReactDOM.render(
   <App />,
